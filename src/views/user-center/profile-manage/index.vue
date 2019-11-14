0ab8bc04b3b45e3c1893c1e7e968f796f8cb1700
lilllj <413775061@qq.com>
2019-11-14 09:45:27 +0800

    feat: 完成个人信息页面部分功能

--- src/views/user-center/profile-manage/index.vue
+++ src/views/user-center/profile-manage/index.vue
@@ -1,12 +1,61 @@
 <template>
+<!-- 个人资料管理页面 -->
   <div class="profile-manage">
-    个人资料管理
+    <!-- 修改密码按钮 -->
+    <el-button class="p1-button" type="primary">修改密码</el-button>
+    <!-- 学生个人信息 -->
+    <div id="p1-test" class="test">
+      <p>姓名</p>
+    </div>
+    <div class="test">
+      <p>{{name}}</p>
+    </div>
+    <div id="p1-test" class="test">
+      <p>学号</p>
+    </div>
+    <div class="test">
+      <p>{{account}}</p>
+    </div>
+    <div id="p1-test" class="test">
+      <p>联系方式</p>
+    </div>
+    <div class="test">
+      <p>{{contact}}</p>
+    </div>
+    <div id="p1-test" class="test">
+      <p>宿舍</p>
+    </div>
+    <div class="test">
+      <p>{{dormitory}}</p>
+    </div>
+    <div id="p1-test" class="test">
+      <p>性别</p>
+    </div>
+    <div class="test">
+      <p>{{sex}}</p>
+    </div>
+    <!-- 学生个人信息【完】 -->
   </div>
 </template>
 
 <script>
+import { userApi } from '@/api'
+import { Button, Message  } from 'element-ui'
 export default {
-    name: 'ProfileManage'
+    name: 'ProfileManage',
+    components: {
+		[Button.name]: Button,
+		[Message.name]: Message
+	},
+	created () {
+        this.$axios.post(prefix.api + userApi.profileManage).then((response)=>{
+            // if(!responseHandler(response.data, this)){
+            //     // 在这里处理错误
+            //     Message.error('请求失败')
+            // }
+            // Message.success('请求成功')
+        })
+    }
 }
 </script>
 
